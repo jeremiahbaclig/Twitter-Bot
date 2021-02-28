f52ae13b5d0060f81921d3b5c14d296e2ba82f38
Jeremiah Baclig <55364674+jeremiahbaclig@users.noreply.github.com>
2021-02-27 20:58:47 -0500

    added hourly posting

--- pokebot.py
+++ pokebot.py
@@ -20,7 +20,7 @@ def check_mentions(api, since_id, all_names):
                     logger.info("Valid Pokemon found - sending")
                     user = api.get_user(tweet.user.id)
                     api.update_status(
-                        status=most_recent(value.lower(), user.screen_name),
+                        status=most_recent(value.lower(), user.screen_name, get_reddit()),
                         in_reply_to_status_id=tweet.id,
                         exclude_reply_user_ids=1357865981155098624
                     )
@@ -37,13 +37,7 @@ def check_mentions(api, since_id, all_names):
         return new_since_id
 
 
-def most_recent(name, user):
-    reddit = praw.Reddit(
-        client_id=environ["API_ID"],
-        client_secret=environ["API_SECRET"],
-        user_agent="<twitter>:<pokeraidapp>:<1.01> (by u/<ming0_>)"
-    )
-
+def most_recent(name, user, reddit):
     Flag = True
     subreddit = reddit.subreddit("pokemongoraids")
     for submission in subreddit.new(limit=10):
@@ -63,15 +57,38 @@ def most_recent(name, user):
     if Flag is False:
         return_string = ""
         return_string = "@%s New raid not found for %s. :( Try again later." % (user, name.upper())
-        
+
     return return_string
 
 
+def print_hourly(api, reddit, time_counter):
+    if datetime.datetime.now().hour == time_counter:
+        subreddit = reddit.subreddit("pokemongoraids")
+        for submission in subreddit.new(limit=1):
+            latest = "%s\n" % submission.url
+
+        api.update_status("MOST RECENT RAID (%d/24): %s" % (time_counter, latest))
+        return time_counter + 1
+    if time_counter == 24:
+        time_counter = 1
+
+    return time_counter
+
+
+def get_reddit():
+    reddit = praw.Reddit(
+        client_id=API_ID,
+        client_secret=API_SECRET,
+        user_agent="<twitter>:<pokeraidapp>:<1.01> (by u/<ming0_>)"
+    )
+    return reddit
+
+
 def main():
-    auth = tweepy.OAuthHandler(environ["API_KEY"],
-                               environ["API_KEY_SECRET"])
-    auth.set_access_token(environ["ACCESS_TOKEN"],
-                          environ["ACCESS_TOKEN_SECRET"])
+    auth = tweepy.OAuthHandler(API_KEY,
+                               API_KEY_SECRET)
+    auth.set_access_token(ACCESS_TOKEN,
+                          ACCESS_TOKEN_SECRET)
 
     api = tweepy.API(auth, wait_on_rate_limit=True,
                      wait_on_rate_limit_notify=True)
@@ -79,6 +96,7 @@ def main():
     df = pandas.read_csv('pokemon.csv')
     all_pokemon = df.values.tolist()
 
+    time_counter = datetime.datetime.now().hour
     since_id = 1
     while True:
         try:
